edc2bdd2c505eb76ef24f575b6bde0ca97935823
bellwie <pedrodev503@gmail.com>
2026-01-27 23:21:05 -0300

    nada

--- react-examples/src/components/ListRender.jsx
+++ react-examples/src/components/ListRender.jsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
 
 const ListRender = () => {
-  const [list] = useState(["pedro", "matheus", "jose"]);
+  const [list, setList] = useState([
+    { id: 1, name: "pedro" },
+    { id: 2, name: "jose" },
+    { id: 3, name: "Lucas" },
+  ]);
 
   return (
     <>
       <ul>
         {list.map((item) => (
-          <li>{item}</li>
+          <li key={item.id}>{item.name}</li>
         ))}
       </ul>
     </>
